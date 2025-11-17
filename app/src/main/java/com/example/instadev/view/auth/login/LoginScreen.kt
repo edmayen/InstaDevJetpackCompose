9e76f2be4e3c7e2c3046a93d86dd2a73b37d6853
Eduardo Mayen <ed_mayen@hotmail.com>
2025-11-17 16:33:59 -0600

    first commit

---
+++ app/src/main/java/com/example/instadev/view/auth/login/LoginScreen.kt
@@ -0,0 +1,125 @@
+package com.example.instadev.view.auth.login
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.instadev.R
+
+@Preview
+@Composable
+fun LoginScreen() {
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Scaffold { paddingValues ->
+        Column(
+            modifier = Modifier
+                .background(Color.White)
+                .padding(paddingValues)
+                .padding(horizontal = 24.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier
+                    .padding(top = 24.dp),
+                text = "Español (España)",
+                color = Color.Gray
+            )
+            Spacer(Modifier.weight(1f))
+            Image(
+                modifier = Modifier
+                    .size(56.dp),
+                painter = painterResource(R.drawable.instadev_logo),
+                contentDescription = "InstaDev logo header"
+            )
+            Spacer(Modifier.weight(1.3f))
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(30),
+                value = email,
+                label = { Text("Usuario, correo electrónico o móvil")},
+                onValueChange = {
+                    email = it
+                }
+            )
+            Spacer(Modifier.height(12.dp))
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(30),
+                value = password,
+                label = { Text("Contraseña")},
+                onValueChange = {
+                    password = it
+                }
+            )
+            Spacer(Modifier.height(12.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Blue
+                ),
+                onClick = {}
+            ) {
+                Text(
+                    modifier = Modifier.padding(vertical = 4.dp),
+                    text = "Iniciar sesión",
+                    color = Color.White
+                )
+            }
+            TextButton(onClick = {}) {
+                Text(
+                    text = "Has olvidado la contraseña?"
+                )
+            }
+            Spacer(Modifier.weight(1f))
+            OutlinedButton (
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {}
+            ) {
+                Text(
+                    modifier = Modifier.padding(vertical = 4.dp),
+                    text = "Crear nueva cuenta"
+                )
+            }
+            Icon(
+                modifier = Modifier
+                    .width(60.dp)
+                    .padding(vertical = 24.dp),
+                painter = painterResource(R.drawable.ic_meta),
+                contentDescription = "Meta logo footer",
+                tint = Color.Gray
+            )
+        }
+    }
+}
